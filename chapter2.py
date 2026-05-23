e2b79c3d61b9a8b032bfe86ec594e6c68a245ca4
Md. Asikur Rahman <ashikurrahman02468@gmail.com>
2026-05-23 11:57:32 +0600

    intro of my family and some basic for str

---
+++ chapter2.py
@@ -0,0 +1,19 @@
+str0 = "Introduction to My Family"
+str1 = "Md.Asikur Rahman"
+str2 = "Md.Abdur Rahman"
+str3 = "Ayesha Akter"
+str4 = "Fatema Akter"
+print(str0)
+print("My name is", str1)
+print("My father's name is", str2)
+print("My mother's name is", str3)
+print("My sister's name is", str4)
+
+#working with string
+strA = "Apna "
+strB = "College"
+strC = strA + strB
+print(strC)
+print (strA + strB)
+print(strA+" "+strB)
+
